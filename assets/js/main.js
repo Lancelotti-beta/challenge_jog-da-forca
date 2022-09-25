5723bbb6d6dd68b028e133c98453b8f6a811c148
Bruno José Marques Da Silva <78493454+Lancelotti-beta@users.noreply.github.com>
2022-09-25 14:54:33 -0300

    usando parentElement apartir da div

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,29 +2,22 @@
 //sessão inicio
 document.querySelector('.botao__jogo').addEventListener('click', e => {
     e.target.parentElement.style.display = 'none'
-    console.log(e.target.parentElement)
     document.querySelector('.tela_jogo').style.display = 'flex'
-    document.querySelector('.tela_jogo-container').style.display = 'flex'
 })
 
 document.querySelector('.botao__configuracao').addEventListener('click', e => {
     e.target.parentElement.style.display = 'none'
-    console.log(e.target.parentElement)
-    document.querySelector('.tela__configuracao').style.display = 'flex'
-    document.querySelector('.tela__configuracao-container').style.display = 'flex'
+    document.querySelector('.tela_configuracao').style.display = 'flex'
 })
 
 //sessão da configuração
 document.querySelector('.botao__configuracao--jogar').addEventListener('click', e => {
-    e.target.parentElement.style.display = 'none'
-    console.log(e.target.parentElement.querySelector('.tela__configuracao'))
+    document.querySelector('.tela__configuracao-container').parentElement.style.display = 'none'
     document.querySelector('.tela_jogo').style.display = 'flex'
-    document.querySelector('.tela_jogo-container').style.display = 'flex'
 })
 
 document.querySelector('.botao__configuracao--voltar').addEventListener('click', e => {
-    e.target.parentElement.style.display = 'none'
-    console.log(e.target.parentElement)
+    document.querySelector('.tela__configuracao-container').parentElement.style.display = 'none'
     document.querySelector('.tela__inicio').style.display = 'flex'
 })
 
@@ -35,9 +28,7 @@ document.querySelector('.bota__jogo--jogar').addEventListener('click', e => {
 })
 */
 document.querySelector('.bota__jogo--voltar').addEventListener('click', e => {
-    e.target.parentElement.style.display = 'none'
-    console.log(e.target.parentElement)
+    document.querySelector('.tela_jogo-container').parentElement.style.display = 'none'
     document.querySelector('.tela__inicio').style.display = 'flex'
-
 })
 
