501cf9574c98f215bb548f805e121dc4aec28b40
Bruno José Marques Da Silva <78493454+Lancelotti-beta@users.noreply.github.com>
2022-09-25 12:52:22 -0300

    adicionando evento ao botão

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,12 @@ const iniciar = document.querySelector('.botao__jogo')
 const adicionaPalavra = document.querySelector('.botao__configuracao')
 
 iniciar.addEventListener('click', function(e){
-    console.log(e.target.parentElement)
+    e.target.parentElement.style.display = 'none'
+    document.querySelector('.tela_jogo').style.display = 'flex'
 })
 
 adicionaPalavra.addEventListener('click', function(e){
-    console.log(e.target.parentElement)
+    e.target.parentElement.style.display = 'none'
+    document.querySelector('.tela__configuracao').style.display = 'flex'
 })
 
