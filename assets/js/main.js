4a0903c5f0f05d02f9d6f09e65a48f2905748af5
Bruno José Marques Da Silva <78493454+Lancelotti-beta@users.noreply.github.com>
2022-09-25 12:39:00 -0300

    testando elementos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
+const iniciar = document.querySelector('.botao__jogo')
+const adicionaPalavra = document.querySelector('.botao__configuracao')
+
+iniciar.addEventListener('click', function(e){
+    console.log(e.target.parentElement)
+})
+
+adicionaPalavra.addEventListener('click', function(e){
+    console.log(e.target.parentElement)
+})
 
