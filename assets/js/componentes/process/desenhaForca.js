a03a6f07c1a14a07aa5b3e09c5f6d6754038a04c
Lancelotti-beta <brunojosemarques.pe@gmail.com>
2022-11-20 12:55:58 -0300

    Inciando a criação das funções partes do corpo

--- assets/js/componentes/process/desenhaForca.js
+++ assets/js/componentes/process/desenhaForca.js
@@ -12,11 +12,51 @@ function desenharCanvas(){
 function desenhaForca(){
     console.log(`Isso é o THIS :\n${this.naturalWidth}\nEsse é o Objeto Canvas:\n${modulo.canvas.largura}`)
     console.log(`Isso é o THIS :\n${this.naturalHeight}\nEsse é o Objeto Canvas:\n${modulo.canvas.altura}`)
-
-    //modulo.canvas.largura = this.naturalWidth;
-    //modulo.canvas.altura = this.naturalHeight;
-
+    
     tela.drawImage(this,  modulo.canvas.largura/3, modulo.canvas.altura/4, this.naturalWidth, this.naturalHeight);
+}
+
+function desenhaCabeca(){
+    const parte = new Image();
+    parte.src = "../../assets/img/cabeca_boneco.svg";
+    parte.onload = null;
+
+    //tela.drawImage(this,  modulo.canvas.largura/3, modulo.canvas.altura/4, this.naturalWidth, this.naturalHeight);
+}
+function desenhaTronco(){
+    const parte = new Image();
+    parte.src = "../../assets/img/tronco_boneco.svg";
+    parte.onload = null;
+
+    //tela.drawImage(this,  modulo.canvas.largura/3, modulo.canvas.altura/4, this.naturalWidth, this.naturalHeight);
+}
+function desenhaBracoDireito(){
+    const parte = new Image();
+    parte.src = "../../assets/img/bracoDireito_boneco.svg";
+    parte.onload = null;
+
+    //tela.drawImage(this,  modulo.canvas.largura/3, modulo.canvas.altura/4, this.naturalWidth, this.naturalHeight);
+}
+function desenhaBracoEsquerdo(){
+    const parte = new Image();
+    parte.src = "../../assets/img/bracoEsquerdo_boneco.svg";
+    parte.onload = null;
+
+    //tela.drawImage(this,  modulo.canvas.largura/3, modulo.canvas.altura/4, this.naturalWidth, this.naturalHeight);
+}
+function desenhaPernaDireita(){
+    const parte = new Image();
+    parte.src = "../../assets/img/pernaDireita_boneco.svg";
+    parte.onload = null;
+
+    //tela.drawImage(this,  modulo.canvas.largura/3, modulo.canvas.altura/4, this.naturalWidth, this.naturalHeight);
+}
+function desenhaPernaEsquerda(){
+    const parte = new Image();
+    parte.src = "../../assets/img/pernaEsquerda_boneco.svg";
+    parte.onload = null;
+
+    //tela.drawImage(this,  modulo.canvas.largura/3, modulo.canvas.altura/4, this.naturalWidth, this.naturalHeight);
 }
 
 export default desenharCanvas
